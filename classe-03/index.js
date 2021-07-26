bfa335031857c2ea05d38f4ee2b4a9ed454e4253
JeanSisse <tavares1988@live.com>
2021-07-26 17:49:41 -0300

    Exercicio classe-03 resolvido

--- classe-03/index.js
+++ classe-03/index.js
@@ -1,5 +1,14 @@
+const express = require('express');
 
+const app = express();
 
-
+let nVez = 0;
 
 const jogadores = ["José", "Maria", "João", "Marcos", "Fernanda"];
+
+app.get("/", (requisicao, resposta) => {
+	resposta.send(`É a vez de ${jogadores[nVez]} jogar!`);
+	nVez = nVez < jogadores.length - 1 ? (++nVez) : 0;
+});
+
+app.listen(8000);
